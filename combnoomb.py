56ac63bc5036425dbdc5e5f49ccdb591a92955e0
Nikhil Munagala <nikhilsai.munagala@gmail.com>
2022-06-09 10:48:43 -0400

    Still working on rref method.

--- combnoomb.py
+++ combnoomb.py
@@ -2,13 +2,13 @@ import array
 import collections
 from collections import deque
 import asyncio
+from pickle import FALSE
 from turtle import dot
 import discord
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
 import numpy as np
-
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 GUILD = os.getenv('ID')
@@ -30,6 +30,11 @@ async def on_message(message):
 async def hello(ctx):
     userName = ctx.message.author.name
     await ctx.reply(f'Hello {userName}!')
+
+@bot.command()
+async def help(ctx):
+    await ctx.send('Hey, which method do you need help on?')
+    anotherMessage = await bot.wait_for('message',check=lambda m: m.author==ctx.author and m.channel==ctx.channel,timeout=80)
 
 @bot.command()
 async def createMatrix(ctx, length:int, width:int, listOfEntries:str, matrixUsed):
@@ -129,6 +134,7 @@ async def matrixVectorProduct(ctx, length:int, width:int):
     matrix = []
     vector = []
     await createMatrix(ctx, length, width, messageUsed, matrix)
+    await ctx.send(f'The rank of the matrix is {rankOfMatrix(matrix)}')
     await ctx.send('Enter a list of entries for the n * 1 vector.')
     anotherMessage = await bot.wait_for('message',check=lambda m: m.author==ctx.author and m.channel==ctx.channel,timeout=80)
     await createVector(ctx, width,anotherMessage.content, vector)
@@ -282,5 +288,105 @@ def eigenvectorOrNot(vector, resultingVector):
     # Return true(considering if the vector is an eigenvector of the matrix or not.)
    return True
 def rankOfMatrix(matrix):
+    # Call the rrefOfMatrix method.
+    # Initialize the rank to 0
+    # Using that matrix, starting the pivot entry at row 0 and col 0.
+        # While the row or col isn't out of bounds. 
+            # If the pivot entry is a 0.
+                # Increment the row first and see if you found a non-zero pivot entry.
+                    # IF yes then increase the rank by 1 and stop searching.
+                    # Otherwise keep looking.
+                # If still not found, then increment the row and col by 1. 
+            # Otherwise
+                # Increment the rank by 1.
+                # Increment the row and col by 1. 
+    # Return the rank of the matrix. 
+
+    return None
+def rrefOfMatrix(matrix):
+    # Stack of indices of pivot entries.
+    stackOfPivotEntries = []
+    # Find the first pivot entry(Normally at first entry of matrix.)
+    col = 0
+    # For each row
+    for i in range(len(matrix)):
+        # If the pivot entry is a 0
+        if matrix[i][col] == 0:
+            # Linearly check the rows below to see if there is another pivot entry below the 0.
+                pivotFound = False
+                for j in range (i + 1, len(matrix)):
+                # If yes, then exchange the rows. 
+                    if(matrix[j][col] != 0):
+                        pivotFound = True
+                        array = matrix[i]
+                        matrix[i] = matrix[j]
+                        matrix[j] = array
+                        break
+                # Otherwise, check each column in the row you are on.
+                beginning = col
+                if(pivotFound == False):
+                    for j in range (col + 1, len(matrix[i])):
+                # If yes, then exchange the rows. 
+                        if(matrix[i][j] != 0):
+                            pivotFound = True
+                            break
+                        col = j
+                    # IF you found a pivot entry, rref starts from there.
+                    if(pivotFound == True):
+                        continue
+                    # Otherwise, pivot entry starts from 1 row down and 1 col right. 
+                    else:
+                        col = beginning
+        # Otherwise
+        else:
+            # IF the pivot entry is not a 1
+            if(matrix[i][col] != 1):
+                # Scale the row by the reciprocal of that entry.
+                for j in range (len(matrix[i])):
+                    matrix[i][j] = (1/matrix[i][col]) * matrix[i][j]
+            # For each row below the pivot entry.
+            for j in range(i + 1, len(matrix)):
+                entryBelowPivot = matrix[j][col]
+                # If the entry below the pivot is 0
+                if(matrix[j][col] == 0):
+                    # Skip and move a row below.
+                    continue
+                # Scale the row containing the pivot you are currently on by -1 * current entry below the pivot entry
+                # and add it to the row that you wanna do the operation on, which is the row containing the current entry
+                # below the pivot entry.
+                m = len(matrix[j])
+                for k in range(m):
+                    matrix[j][k] = matrix[j][k] + (-1 * entryBelowPivot) * matrix[i][k]
+        # Make sure you push i and j in the stack before moving on to next iteration.
+        stackOfPivotEntries.append(i)
+        stackOfPivotEntries.append(col)
+        col = col + 1
+    # And then do the steps above but backwards essentially. 
+    # While the stack is not empty
+    while(len(stackOfPivotEntries) != 0):
+        # Col number is the first dequeued.
+        colNum = stackOfPivotEntries.pop()
+        # Row number is the second dequeued.
+        rowNum = stackOfPivotEntries.pop()
+        # Using that row and col number, that is where the pivot entry is located in. 
+        # For each row above the pivot entry
+        for i in range(rowNum - 1, 0):
+            # # If the pivot entry is a 0
+            if(matrix[rowNum][colNum] == 0):
+            # Linearly check the rows below to see if there is another pivot entry below the 0.
+                # If yes, then exchange the rows. 
+                # Otherwise, check each column in the row you are on.
+                    # IF you found a pivot entry, rref starts from there.
+                    # Otherwise, pivot entry starts from 1 row down and 1 col right. 
+        # Otherwise
+            # IF the pivot entry is not a 1
+                # Scale the row by the reciprocal of that entry.
+            # For each row below the pivot entry.
+                # If the pivot is 0
+                    # Skip and move a row below.
+                # Scale the row containing the pivot you are currently on by -1 * current entry below the pivot entry
+                # and add it to the row that you wanna do the operation on, which is the row containing the current entry
+                # below the pivot entry.
+    # Return the matrix at the end. 
     return None
 bot.run(TOKEN)
